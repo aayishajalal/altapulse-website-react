c93ce0ddb49495941675fa5f9ae4e759749eda77
Aayisha Noora Jalaludeen <aayishajalal05@gmail.com>
2024-09-19 23:04:59 +0530

    updated-services

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -15,10 +15,38 @@ const Services = () => {
       <HeroSection />
       <About />
       <WhyChooseUs />
-      <RightContentService />
-      <LeftContentService />
-      <RightContentService />
-      <LeftContentService />
+      <RightContentService
+        title="Marketing and Automation"
+        description="Arcu at dictum sapien, mollis. Vulputate sit id accumsan, ultricies. In ultrices malesuada elit mauris etiam odio. Duis tristique lacus, et blandit viverra nisl velit. Sed mattis rhoncus, diam suspendisse sit nunc, gravida eu. Lectus eget eget ac dolor neque lorem sapien, suspendisse aliquam."
+        image_url="https://ik.imagekit.io/yuq4cit8f/Editing.svg?updatedAt=1724916734902"
+        image_alt="Editing"
+        icon_url="https://ik.imagekit.io/yuq4cit8f/point-star-white.svg?updatedAt=1725597033557"
+        icon_alt="point-star-white"
+      />
+      <LeftContentService
+        title="Marketing and Automation"
+        description="Arcu at dictum sapien, mollis. Vulputate sit id accumsan, ultricies. In ultrices malesuada elit mauris etiam odio. Duis tristique lacus, et blandit viverra nisl velit. Sed mattis rhoncus, diam suspendisse sit nunc, gravida eu. Lectus eget eget ac dolor neque lorem sapien, suspendisse aliquam."
+        image_url="https://ik.imagekit.io/yuq4cit8f/Editing.svg?updatedAt=1724916734902"
+        image_alt="Editing"
+        icon_url="https://ik.imagekit.io/yuq4cit8f/point-star-white.svg?updatedAt=1725597033557"
+        icon_alt="point-star-white"
+      />
+      <RightContentService
+        title="Marketing and Automation"
+        description="Arcu at dictum sapien, mollis. Vulputate sit id accumsan, ultricies. In ultrices malesuada elit mauris etiam odio. Duis tristique lacus, et blandit viverra nisl velit. Sed mattis rhoncus, diam suspendisse sit nunc, gravida eu. Lectus eget eget ac dolor neque lorem sapien, suspendisse aliquam."
+        image_url="https://ik.imagekit.io/yuq4cit8f/Editing.svg?updatedAt=1724916734902"
+        image_alt="Editing"
+        icon_url="https://ik.imagekit.io/yuq4cit8f/point-star-white.svg?updatedAt=1725597033557"
+        icon_alt="point-star-white"
+      />
+      <LeftContentService
+        title="Marketing and Automation"
+        description="Arcu at dictum sapien, mollis. Vulputate sit id accumsan, ultricies. In ultrices malesuada elit mauris etiam odio. Duis tristique lacus, et blandit viverra nisl velit. Sed mattis rhoncus, diam suspendisse sit nunc, gravida eu. Lectus eget eget ac dolor neque lorem sapien, suspendisse aliquam."
+        image_url="https://ik.imagekit.io/yuq4cit8f/Editing.svg?updatedAt=1724916734902"
+        image_alt="Editing"
+        icon_url="https://ik.imagekit.io/yuq4cit8f/point-star-white.svg?updatedAt=1725597033557"
+        icon_alt="point-star-white"
+      />
       <FAQ />
       <Banner />
     </>
