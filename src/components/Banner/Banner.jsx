824f518bf6829a7ee0f7a84f7a635cf26bed6394
Aayisha Noora Jalaludeen <aayishajalal05@gmail.com>
2024-09-19 21:34:39 +0530

    banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,11 +1,45 @@
 import React from "react";
+import { FillButton } from "../Button/Button";
 
 const Banner = () => {
   return (
     <>
       <div className="2xl:container mx-auto">
-        <div className="w-[90%] mx-auto ">
-          <div></div>
+        <div className="w-[90%] mx-auto">
+          <div className="flex flex-col md:flex-row bg-black rounded-xl">
+            {/* Image Section */}
+            <div className="flex items-end justify-start md:justify-end  md:p-2">
+              <img
+                src="https://ik.imagekit.io/yuq4cit8f/ThreeCircle.svg?updatedAt=1726394815864"
+                alt=""
+                className="h-32 md:h-[32rem] lg:h-80"
+              />
+            </div>
+            {/* Text Section */}
+            <div className="flex flex-col justify-center text-white gap-3 p-10 md:p-20 lg:p-32 w-full md:w-3/4">
+              <h1 className="text-2xl sm:text-2xl md:text-3xl lg:text-4xl font-bold">
+                Ready To Get Started?
+              </h1>
+              <p className="text-sm sm:text-base md:text-lg lg:text-lg">
+                Risus habitant leo egestas mauris diam eget morbi tempus
+                vulputate.
+              </p>
+              {/* Adjusted Button */}
+              <div className="mt-4">
+                <FillButton name="Contact" className="max-w-max" />{" "}
+                {/* Ensure button fits within content */}
+              </div>
+            </div>
+
+            {/* Image Section */}
+            <div className="flex items-end justify-end px-2 md:justify-end w-full md:w-1/2">
+              <img
+                src="https://ik.imagekit.io/yuq4cit8f/colorphone.svg?updatedAt=1726393017300"
+                alt=""
+                className="h-64 md:h-[32rem] lg:h-96"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </>
